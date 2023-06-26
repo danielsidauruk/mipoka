39011353e2df90c78bfb232aead59dd7aa7c60f7
danielsidauruk <danielsidauruk11@gmail.com>
2023-06-26 23:17:07 +0700

    Add RiwayatMptMode Model & Entity

--- lib/mipoka/data/models/riwayat_mpt_model.dart
+++ lib/mipoka/data/models/riwayat_mpt_model.dart
@@ -1,78 +1,65 @@
-import 'package:mipoka/mipoka/data/models/kegiatan_model.dart';
-import 'package:mipoka/mipoka/data/models/user_model.dart';
 import 'package:mipoka/mipoka/domain/entities/riwayat_mpt.dart';
 
-// => Just Updated
 class RiwayatMptModel extends RiwayatMpt {
-  final KegiatanModel kegiatanModel;
-  final MipokaUserModel userModel;
-
   const RiwayatMptModel({
     required super.idRiwayatMpt,
-    required this.kegiatanModel,
-    required this.userModel,
-    required super.status,
-    required super.file,
+    required super.idKegiatanMpt,
+    required super.idUser,
+    required super.statusMpt,
+    required super.fileSertifikatMpt,
     required super.hash,
-    required super.keteranganA,
-    required super.keteranganB,
-    required super.mulai,
-    required super.akhir,
-    required super.updatedAt,
+    required super.keteranganMhs,
+    required super.keteranganSa,
     required super.createdAt,
-  }) : super(
-    kegiatan: kegiatanModel,
-    user: userModel,
-  );
+    required super.createdBy,
+    required super.updatedAt,
+    required super.updatedBy,
+  });
 
   factory RiwayatMptModel.fromEntity(RiwayatMpt riwayatMpt) {
     return RiwayatMptModel(
       idRiwayatMpt: riwayatMpt.idRiwayatMpt,
-      kegiatanModel: KegiatanModel.fromEntity(riwayatMpt.kegiatan),
-      userModel: MipokaUserModel.fromEntity(riwayatMpt.user),
-      status: riwayatMpt.status,
-      file: riwayatMpt.file,
+      idKegiatanMpt: riwayatMpt.idKegiatanMpt,
+      idUser: riwayatMpt.idUser,
+      statusMpt: riwayatMpt.statusMpt,
+      fileSertifikatMpt: riwayatMpt.fileSertifikatMpt,
       hash: riwayatMpt.hash,
-      keteranganA: riwayatMpt.keteranganA,
-      keteranganB: riwayatMpt.keteranganB,
-      mulai: riwayatMpt.mulai,
-      akhir: riwayatMpt.akhir,
-      updatedAt: riwayatMpt.updatedAt,
+      keteranganMhs: riwayatMpt.keteranganMhs,
+      keteranganSa: riwayatMpt.keteranganSa,
       createdAt: riwayatMpt.createdAt,
+      createdBy: riwayatMpt.createdBy,
+      updatedAt: riwayatMpt.updatedAt,
+      updatedBy: riwayatMpt.updatedBy,
     );
   }
 
-  factory RiwayatMptModel.fromJson(Map<String, dynamic> json) {
-    return RiwayatMptModel(
-      idRiwayatMpt: json['idRiwayatMpt'],
-      kegiatanModel: KegiatanModel.fromJson(json['kegiatan']),
-      userModel: MipokaUserModel.fromJson(json['user']),
-      status: json['status'],
-      file: json['file'],
-      hash: json['hash'],
-      keteranganA: json['keteranganA'],
-      keteranganB: json['keteranganB'],
-      mulai: json['mulai'],
-      akhir: json['akhir'],
-      updatedAt: json['updatedAt'],
-      createdAt: json['createdAt'],
-    );
-  }
+  factory RiwayatMptModel.fromJson(Map<String, dynamic> json) => RiwayatMptModel(
+    idRiwayatMpt: json["id_riwayat_mpt"],
+    idKegiatanMpt: json["id_kegiatan_mpt"],
+    idUser: json["id_user"],
+    statusMpt: json["status_mpt"],
+    fileSertifikatMpt: json["file_sertifikat_mpt"],
+    hash: json["hash"],
+    keteranganMhs: json["keterangan_mhs"],
+    keteranganSa: json["keterangan_sa"],
+    createdAt: json["created_at"],
+    createdBy: json["created_by"],
+    updatedAt: json["updated_at"],
+    updatedBy: json["updated_by"],
+  );
 
-  Map<String, dynamic> toJson() {
-    return {
-      'idRiwayatMpt': idRiwayatMpt,
-      'kegiatan': kegiatanModel.toJson(),
-      'user': userModel.toJson(),
-      'status': status,
-      'file': file,
-      'hash': hash,
-      'keteranganA': keteranganA,
-      'keteranganB': keteranganB,
-      'mulai': mulai,
-      'akhir': akhir,
-      'updatedAt': updatedAt,
-      'createdAt': createdAt,
-    };
-  }
+  Map<String, dynamic> toJson() => {
+    "id_riwayat_mpt": idRiwayatMpt,
+    "id_kegiatan_mpt": idKegiatanMpt,
+    "id_user": idUser,
+    "status_mpt": statusMpt,
+    "file_sertifikat_mpt": fileSertifikatMpt,
+    "hash": hash,
+    "keterangan_mhs": keteranganMhs,
+    "keterangan_sa": keteranganSa,
+    "created_at": createdAt,
+    "created_by": createdBy,
+    "updated_at": updatedAt,
+    "updated_by": updatedBy,
+  };
 }
--- lib/mipoka/domain/entities/riwayat_mpt.dart
+++ lib/mipoka/domain/entities/riwayat_mpt.dart
@@ -1,79 +1,75 @@
 import 'package:equatable/equatable.dart';
-import 'package:mipoka/mipoka/domain/entities/kegiatan_mpt.dart';
-import 'package:mipoka/mipoka/domain/entities/user.dart';
 
-// => Just Updated
 class RiwayatMpt extends Equatable {
   final int idRiwayatMpt;
-  final Kegiatan kegiatan;
-  final MipokaUser user;
-  final String status;
-  final String file;
+  final int idKegiatanMpt;
+  final String idUser;
+  final String statusMpt;
+  final String fileSertifikatMpt;
   final String hash;
-  final String keteranganA;
-  final String keteranganB;
-  final DateTime mulai;
-  final DateTime akhir;
-  final DateTime updatedAt;
-  final DateTime createdAt;
+  final String keteranganMhs;
+  final String keteranganSa;
+  final String createdAt;
+  final String createdBy;
+  final String updatedAt;
+  final String updatedBy;
 
   const RiwayatMpt({
     required this.idRiwayatMpt,
-    required this.kegiatan,
-    required this.user,
-    required this.status,
-    required this.file,
+    required this.idKegiatanMpt,
+    required this.idUser,
+    required this.statusMpt,
+    required this.fileSertifikatMpt,
     required this.hash,
-    required this.keteranganA,
-    required this.keteranganB,
-    required this.mulai,
-    required this.akhir,
-    required this.updatedAt,
+    required this.keteranganMhs,
+    required this.keteranganSa,
     required this.createdAt,
+    required this.createdBy,
+    required this.updatedAt,
+    required this.updatedBy,
   });
 
   RiwayatMpt copyWith({
     int? idRiwayatMpt,
-    Kegiatan? kegiatan,
-    MipokaUser? user,
-    String? status,
-    String? file,
+    int? idKegiatanMpt,
+    String? idUser,
+    String? statusMpt,
+    String? fileSertifikatMpt,
     String? hash,
-    String? keteranganA,
-    String? keteranganB,
-    DateTime? mulai,
-    DateTime? akhir,
-    DateTime? updatedAt,
-    DateTime? createdAt,
+    String? keteranganMhs,
+    String? keteranganSa,
+    String? createdAt,
+    String? createdBy,
+    String? updatedAt,
+    String? updatedBy,
   }) =>
       RiwayatMpt(
         idRiwayatMpt: idRiwayatMpt ?? this.idRiwayatMpt,
-        kegiatan: kegiatan ?? this.kegiatan,
-        user: user ?? this.user,
-        status: status ?? this.status,
-        file: file ?? this.file,
+        idKegiatanMpt: idKegiatanMpt ?? this.idKegiatanMpt,
+        idUser: idUser ?? this.idUser,
+        statusMpt: statusMpt ?? this.statusMpt,
+        fileSertifikatMpt: fileSertifikatMpt ?? this.fileSertifikatMpt,
         hash: hash ?? this.hash,
-        keteranganA: keteranganA ?? this.keteranganA,
-        keteranganB: keteranganB ?? this.keteranganB,
-        mulai: mulai ?? this.mulai,
-        akhir: akhir ?? this.akhir,
-        updatedAt: updatedAt ?? this.updatedAt,
+        keteranganMhs: keteranganMhs ?? this.keteranganMhs,
+        keteranganSa: keteranganSa ?? this.keteranganSa,
         createdAt: createdAt ?? this.createdAt,
+        createdBy: createdBy ?? this.createdBy,
+        updatedAt: updatedAt ?? this.updatedAt,
+        updatedBy: updatedBy ?? this.updatedBy,
       );
 
   @override
   List<Object?> get props => [
-    idRiwayatMpt,
-    kegiatan,
-    user,
-    status,
-    file,
+    idKegiatanMpt,
+    idUser,
+    statusMpt,
+    fileSertifikatMpt,
     hash,
-    keteranganA,
-    keteranganB,
-    mulai,
-    akhir,
-    updatedAt,
+    keteranganMhs,
+    keteranganSa,
     createdAt,
+    createdBy,
+    updatedAt,
+    updatedBy,
   ];
 }
