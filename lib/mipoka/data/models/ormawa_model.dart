e83b5114f647de00f80c53f1d92b75309b02776a
danielsidauruk <danielsidauruk11@gmail.com>
2023-06-26 22:36:07 +0700

    Add Ormawa Model & Entity

--- lib/mipoka/data/models/ormawa_model.dart
+++ lib/mipoka/data/models/ormawa_model.dart
@@ -1,19 +1,24 @@
 import 'package:mipoka/mipoka/domain/entities/ormawa.dart';
 
-// => Just Updated
 class OrmawaModel extends Ormawa {
-  OrmawaModel({
+  const OrmawaModel({
     required super.idOrmawa,
     required super.namaOrmawa,
     required super.pembina,
     required super.ketua,
     required super.wakil,
+    required super.sekretaris,
     required super.bendahara,
     required super.jumlahAnggota,
     required super.fotoPembina,
     required super.fotoKetua,
     required super.fotoWakil,
+    required super.fotoSekretaris,
     required super.fotoBendahara,
+    required super.createdAt,
+    required super.createdBy,
+    required super.updatedBy,
+    required super.updatedAt,
   });
 
   factory OrmawaModel.fromEntity(Ormawa ormawa) {
@@ -23,40 +28,58 @@ class OrmawaModel extends Ormawa {
       pembina: ormawa.pembina,
       ketua: ormawa.ketua,
       wakil: ormawa.wakil,
+      sekretaris: ormawa.sekretaris,
       bendahara: ormawa.bendahara,
       jumlahAnggota: ormawa.jumlahAnggota,
       fotoPembina: ormawa.fotoPembina,
       fotoKetua: ormawa.fotoKetua,
       fotoWakil: ormawa.fotoWakil,
+      fotoSekretaris: ormawa.fotoSekretaris,
       fotoBendahara: ormawa.fotoBendahara,
+      createdAt: ormawa.createdAt,
+      createdBy: ormawa.createdBy,
+      updatedAt: ormawa.updatedAt,
+      updatedBy: ormawa.updatedBy,
     );
   }
 
   factory OrmawaModel.fromJson(Map<String, dynamic> json) => OrmawaModel(
-    idOrmawa: json["idOrmawa"],
-    namaOrmawa: json["namaOrmawa"],
+    idOrmawa: json["id_ormawa"],
+    namaOrmawa: json["nama_ormawa"],
     pembina: json["pembina"],
     ketua: json["ketua"],
     wakil: json["wakil"],
+    sekretaris: json["sekretaris"],
     bendahara: json["bendahara"],
-    jumlahAnggota: json["jumlahAnggota"],
-    fotoPembina: json["fotoPembina"],
-    fotoKetua: json["fotoKetua"],
-    fotoWakil: json["fotoWakil"],
-    fotoBendahara: json["fotoBendahara"],
+    jumlahAnggota: json["jumlah_anggota"],
+    fotoPembina: json["foto_pembina"],
+    fotoKetua: json["foto_ketua"],
+    fotoWakil: json["foto_wakil"],
+    fotoSekretaris: json["foto_sekretaris"],
+    fotoBendahara: json["foto_bendahara"],
+    createdAt: json["created_at"],
+    createdBy: json["created_by"],
+    updatedBy: json["updated_by"],
+    updatedAt: json["updated_at"],
   );
 
   Map<String, dynamic> toJson() => {
-    "idOrmawa": idOrmawa,
-    "namaOrmawa": namaOrmawa,
+    "id_ormawa": idOrmawa,
+    "nama_ormawa": namaOrmawa,
     "pembina": pembina,
     "ketua": ketua,
     "wakil": wakil,
+    "sekretaris": sekretaris,
     "bendahara": bendahara,
-    "jumlahAnggota": jumlahAnggota,
-    "fotoPembina": fotoPembina,
-    "fotoKetua": fotoKetua,
-    "fotoWakil": fotoWakil,
-    "fotoBendahara": fotoBendahara,
+    "jumlah_anggota": jumlahAnggota,
+    "foto_pembina": fotoPembina,
+    "foto_ketua": fotoKetua,
+    "foto_wakil": fotoWakil,
+    "foto_sekretaris": fotoSekretaris,
+    "foto_bendahara": fotoBendahara,
+    "created_at": createdAt,
+    "created_by": createdBy,
+    "updated_by": updatedBy,
+    "updated_at": updatedAt,
   };
 }
--- lib/mipoka/domain/entities/ormawa.dart
+++ lib/mipoka/domain/entities/ormawa.dart
@@ -1,31 +1,42 @@
 import 'package:equatable/equatable.dart';
 
-// Just updated
 class Ormawa extends Equatable{
   final int idOrmawa;
   final String namaOrmawa;
   final String pembina;
   final String ketua;
   final String wakil;
+  final String sekretaris;
   final String bendahara;
   final int jumlahAnggota;
   final String fotoPembina;
   final String fotoKetua;
   final String fotoWakil;
+  final String fotoSekretaris;
   final String fotoBendahara;
+  final String createdAt;
+  final String createdBy;
+  final String updatedBy;
+  final String updatedAt;
 
-  Ormawa({
+  const Ormawa({
     required this.idOrmawa,
     required this.namaOrmawa,
     required this.pembina,
     required this.ketua,
     required this.wakil,
+    required this.sekretaris,
     required this.bendahara,
     required this.jumlahAnggota,
     required this.fotoPembina,
     required this.fotoKetua,
     required this.fotoWakil,
+    required this.fotoSekretaris,
     required this.fotoBendahara,
+    required this.createdAt,
+    required this.createdBy,
+    required this.updatedBy,
+    required this.updatedAt,
   });
 
   Ormawa copyWith({
@@ -34,12 +45,18 @@ class Ormawa extends Equatable{
     String? pembina,
     String? ketua,
     String? wakil,
+    String? sekretaris,
     String? bendahara,
     int? jumlahAnggota,
     String? fotoPembina,
     String? fotoKetua,
     String? fotoWakil,
+    String? fotoSekretaris,
     String? fotoBendahara,
+    String? createdAt,
+    String? createdBy,
+    String? updatedBy,
+    String? updatedAt,
   }) =>
       Ormawa(
         idOrmawa: idOrmawa ?? this.idOrmawa,
@@ -47,12 +64,18 @@ class Ormawa extends Equatable{
         pembina: pembina ?? this.pembina,
         ketua: ketua ?? this.ketua,
         wakil: wakil ?? this.wakil,
+        sekretaris: sekretaris ?? this.sekretaris,
         bendahara: bendahara ?? this.bendahara,
         jumlahAnggota: jumlahAnggota ?? this.jumlahAnggota,
         fotoPembina: fotoPembina ?? this.fotoPembina,
         fotoKetua: fotoKetua ?? this.fotoKetua,
         fotoWakil: fotoWakil ?? this.fotoWakil,
+        fotoSekretaris: fotoSekretaris ?? this.fotoSekretaris,
         fotoBendahara: fotoBendahara ?? this.fotoBendahara,
+        createdAt: createdAt ?? this.createdAt,
+        createdBy: createdBy ?? this.createdBy,
+        updatedBy: updatedBy ?? this.updatedBy,
+        updatedAt: updatedAt ?? this.updatedAt,
       );
 
   @override
@@ -62,11 +85,17 @@ class Ormawa extends Equatable{
     pembina,
     ketua,
     wakil,
+    sekretaris,
     bendahara,
     jumlahAnggota,
     fotoPembina,
     fotoKetua,
     fotoWakil,
+    fotoSekretaris,
     fotoBendahara,
+    createdAt,
+    createdBy,
+    updatedBy,
+    updatedAt,
   ];
 }
