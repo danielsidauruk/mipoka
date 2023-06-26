f414fe3d21eaec149b8297c17ce377310c1f9361
danielsidauruk <danielsidauruk11@gmail.com>
2023-06-26 23:01:22 +0700

    Add Prestasi Model & Entity

--- lib/mipoka/data/models/prestasi_model.dart
+++ lib/mipoka/data/models/prestasi_model.dart
@@ -2,51 +2,64 @@ import 'package:mipoka/mipoka/domain/entities/prestasi.dart';
 
 class PrestasiModel extends Prestasi {
   const PrestasiModel({
-    required int idPrestasi,
-    required String nim,
-    required String namaLengkap,
-    required String kegiatan,
-    required String tingkat,
-    required String prestasiDicapai,
-  }) : super(
-    idPrestasi: idPrestasi,
-    nim: nim,
-    namaLengkap: namaLengkap,
-    kegiatan: kegiatan,
-    tingkat: tingkat,
-    prestasiDicapai: prestasiDicapai,
-  );
+    required super.idPrestasi,
+    required super.idOrmawa,
+    required super.idUser,
+    required super.namaKegiatan,
+    required super.waktuPenyelenggaraan,
+    required super.tingkat,
+    required super.prestasiDicapai,
+    required super.unggahSertifikat,
+    required super.createdAt,
+    required super.createdBy,
+    required super.updatedAt,
+    required super.updatedBy,
+  });
 
   factory PrestasiModel.fromEntity(Prestasi prestasi) {
     return PrestasiModel(
       idPrestasi: prestasi.idPrestasi,
-      nim: prestasi.nim,
-      namaLengkap: prestasi.namaLengkap,
-      kegiatan: prestasi.kegiatan,
+      idOrmawa: prestasi.idOrmawa,
+      idUser: prestasi.idUser,
+      namaKegiatan: prestasi.namaKegiatan,
+      waktuPenyelenggaraan: prestasi.waktuPenyelenggaraan,
       tingkat: prestasi.tingkat,
       prestasiDicapai: prestasi.prestasiDicapai,
+      unggahSertifikat: prestasi.unggahSertifikat,
+      createdAt: prestasi.createdAt,
+      createdBy: prestasi.createdBy,
+      updatedAt: prestasi.updatedAt,
+      updatedBy: prestasi.updatedBy,
     );
   }
 
-  factory PrestasiModel.fromJson(Map<String, dynamic> json) {
-    return PrestasiModel(
-      idPrestasi: json['idPrestasi'],
-      nim: json['nim'],
-      namaLengkap: json['namaLengkap'],
-      kegiatan: json['kegiatan'],
-      tingkat: json['tingkat'],
-      prestasiDicapai: json['prestasiDicapai'],
-    );
-  }
+  factory PrestasiModel.fromJson(Map<String, dynamic> json) => PrestasiModel(
+    idPrestasi: json["id_prestasi"],
+    idOrmawa: json["id_ormawa"],
+    idUser: json["id_user"],
+    namaKegiatan: json["nama_kegiatan"],
+    waktuPenyelenggaraan: json["waktu_penyelenggaraan"],
+    tingkat: json["tingkat"],
+    prestasiDicapai: json["prestasi_dicapai"],
+    unggahSertifikat: json["unggah_sertifikat"],
+    createdAt: json["created_at"],
+    createdBy: json["created_by"],
+    updatedAt: json["updated_at"],
+    updatedBy: json["updated_by"],
+  );
 
-  Map<String, dynamic> toJson() {
-    return {
-      'idPrestasi': idPrestasi,
-      'nim': nim,
-      'namaLengkap': namaLengkap,
-      'kegiatan': kegiatan,
-      'tingkat': tingkat,
-      'prestasiDicapai': prestasiDicapai,
-    };
-  }
+  Map<String, dynamic> toJson() => {
+    "id_prestasi": idPrestasi,
+    "id_ormawa": idOrmawa,
+    "id_user": idUser,
+    "nama_kegiatan": namaKegiatan,
+    "waktu_penyelenggaraan": waktuPenyelenggaraan,
+    "tingkat": tingkat,
+    "prestasi_dicapai": prestasiDicapai,
+    "unggah_sertifikat": unggahSertifikat,
+    "created_at": createdAt,
+    "created_by": createdBy,
+    "updated_at": updatedAt,
+    "updated_by": updatedBy,
+  };
 }
--- lib/mipoka/domain/entities/prestasi.dart
+++ lib/mipoka/domain/entities/prestasi.dart
@@ -2,46 +2,75 @@ import 'package:equatable/equatable.dart';
 
 class Prestasi extends Equatable {
   final int idPrestasi;
-  final String nim;
-  final String namaLengkap;
-  final String kegiatan;
+  final int idOrmawa;
+  final String idUser;
+  final String namaKegiatan;
+  final String waktuPenyelenggaraan;
   final String tingkat;
   final String prestasiDicapai;
+  final String unggahSertifikat;
+  final String createdAt;
+  final String createdBy;
+  final String updatedAt;
+  final String updatedBy;
 
   const Prestasi({
     required this.idPrestasi,
-    required this.nim,
-    required this.namaLengkap,
-    required this.kegiatan,
+    required this.idOrmawa,
+    required this.idUser,
+    required this.namaKegiatan,
+    required this.waktuPenyelenggaraan,
     required this.tingkat,
     required this.prestasiDicapai,
+    required this.unggahSertifikat,
+    required this.createdAt,
+    required this.createdBy,
+    required this.updatedAt,
+    required this.updatedBy,
   });
 
   Prestasi copyWith({
     int? idPrestasi,
-    String? nim,
-    String? namaLengkap,
-    String? kegiatan,
+    int? idOrmawa,
+    String? idUser,
+    String? namaKegiatan,
+    String? waktuPenyelenggaraan,
     String? tingkat,
     String? prestasiDicapai,
-  }) {
-    return Prestasi(
-      idPrestasi: idPrestasi ?? this.idPrestasi,
-      nim: nim ?? this.nim,
-      namaLengkap: namaLengkap ?? this.namaLengkap,
-      kegiatan: kegiatan ?? this.kegiatan,
-      tingkat: tingkat ?? this.tingkat,
-      prestasiDicapai: prestasiDicapai ?? this.prestasiDicapai,
-    );
-  }
+    String? unggahSertifikat,
+    String? createdAt,
+    String? createdBy,
+    String? updatedAt,
+    String? updatedBy,
+  }) =>
+      Prestasi(
+        idPrestasi: idPrestasi ?? this.idPrestasi,
+        idOrmawa: idOrmawa ?? this.idOrmawa,
+        idUser: idUser ?? this.idUser,
+        namaKegiatan: namaKegiatan ?? this.namaKegiatan,
+        waktuPenyelenggaraan: waktuPenyelenggaraan ?? this.waktuPenyelenggaraan,
+        tingkat: tingkat ?? this.tingkat,
+        prestasiDicapai: prestasiDicapai ?? this.prestasiDicapai,
+        unggahSertifikat: unggahSertifikat ?? this.unggahSertifikat,
+        createdAt: createdAt ?? this.createdAt,
+        createdBy: createdBy ?? this.createdBy,
+        updatedAt: updatedAt ?? this.updatedAt,
+        updatedBy: updatedBy ?? this.updatedBy,
+      );
 
   @override
   List<Object?> get props => [
     idPrestasi,
-    nim,
-    namaLengkap,
-    kegiatan,
+    idOrmawa,
+    idUser,
+    namaKegiatan,
+    waktuPenyelenggaraan,
     tingkat,
     prestasiDicapai,
+    unggahSertifikat,
+    createdAt,
+    createdBy,
+    updatedAt,
+    updatedBy,
   ];
 }
