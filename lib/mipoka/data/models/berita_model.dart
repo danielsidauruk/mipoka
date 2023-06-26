ee5f4fcd2f42ec83186907b6d978787bc701bc6c
danielsidauruk <danielsidauruk11@gmail.com>
2023-06-26 21:57:13 +0700

    Add Berita Model & Entity

--- lib/mipoka/data/models/berita_model.dart
+++ lib/mipoka/data/models/berita_model.dart
@@ -2,46 +2,56 @@ import 'package:mipoka/mipoka/domain/entities/berita.dart';
 
 class BeritaModel extends Berita {
   const BeritaModel({
-    required int idBerita,
-    required String jenisKegiatan,
-    required String penulis,
-    required String gambar,
-    required String teks,
-  }) : super(
-    idBerita: idBerita,
-    jenisKegiatan: jenisKegiatan,
-    penulis: penulis,
-    gambar: gambar,
-    teks: teks,
-  );
+    required super.idBerita,
+    required super.judul,
+    required super.penulis,
+    required super.gambar,
+    required super.teks,
+    required super.tglTerbit,
+    required super.createdAt,
+    required super.createdBy,
+    required super.updatedAt,
+    required super.updatedBy,
+  });
 
   factory BeritaModel.fromEntity(Berita berita) {
     return BeritaModel(
       idBerita: berita.idBerita,
-      jenisKegiatan: berita.jenisKegiatan,
+      judul: berita.judul,
       penulis: berita.penulis,
       gambar: berita.gambar,
       teks: berita.teks,
+      tglTerbit: berita.tglTerbit,
+      createdAt: berita.createdAt,
+      createdBy: berita.createdBy,
+      updatedAt: berita.updatedAt,
+      updatedBy: berita.updatedBy,
     );
   }
 
-  factory BeritaModel.fromJson(Map<String, dynamic> json) {
-    return BeritaModel(
-      idBerita: json['idBerita'],
-      jenisKegiatan: json['jenisKegiatan'],
-      penulis: json['penulis'],
-      gambar: json['gambar'],
-      teks: json['teks'],
-    );
-  }
+  factory BeritaModel.fromJson(Map<String, dynamic> json) => BeritaModel(
+    idBerita: json["id_berita"],
+    judul: json["judul"],
+    penulis: json["penulis"],
+    gambar: json["gambar"],
+    teks: json["teks"],
+    tglTerbit: json["tgl_terbit"],
+    createdAt: json["created_at"],
+    createdBy: json["created_by"],
+    updatedAt: json["updated_at"],
+    updatedBy: json["updated_by"],
+  );
 
-  Map<String, dynamic> toJson() {
-    return {
-      'idBerita': idBerita,
-      'jenisKegiatan': jenisKegiatan,
-      'penulis': penulis,
-      'gambar': gambar,
-      'teks': teks,
-    };
-  }
+  Map<String, dynamic> toJson() => {
+    "id_berita": idBerita,
+    "judul": judul,
+    "penulis": penulis,
+    "gambar": gambar,
+    "teks": teks,
+    "tgl_terbit": tglTerbit,
+    "created_at": createdAt,
+    "created_by": createdBy,
+    "updated_at": updatedAt,
+    "updated_by": updatedBy,
+  };
 }
--- lib/mipoka/domain/entities/berita.dart
+++ lib/mipoka/domain/entities/berita.dart
@@ -1,43 +1,66 @@
 import 'package:equatable/equatable.dart';
-import 'package:mipoka/mipoka/data/models/berita_model.dart';
 
 class Berita extends Equatable {
   final int idBerita;
-  final String jenisKegiatan;
+  final String judul;
   final String penulis;
   final String gambar;
   final String teks;
+  final String tglTerbit;
+  final String createdAt;
+  final String createdBy;
+  final String updatedAt;
+  final String updatedBy;
 
   const Berita({
     required this.idBerita,
-    required this.jenisKegiatan,
+    required this.judul,
     required this.penulis,
     required this.gambar,
     required this.teks,
+    required this.tglTerbit,
+    required this.createdAt,
+    required this.createdBy,
+    required this.updatedAt,
+    required this.updatedBy,
   });
 
-  Berita updateWith({
+  Berita copyWith({
     int? idBerita,
-    String? jenisKegiatan,
+    String? judul,
     String? penulis,
     String? gambar,
     String? teks,
-  }) {
-    return Berita(
-      idBerita: idBerita ?? this.idBerita,
-      jenisKegiatan: jenisKegiatan ?? this.jenisKegiatan,
-      penulis: penulis ?? this.penulis,
-      gambar: gambar ?? this.gambar,
-      teks: teks ?? this.teks,
-    );
-  }
+    String? tglTerbit,
+    String? createdAt,
+    String? createdBy,
+    String? updatedAt,
+    String? updatedBy,
+  }) =>
+      Berita(
+        idBerita: idBerita ?? this.idBerita,
+        judul: judul ?? this.judul,
+        penulis: penulis ?? this.penulis,
+        gambar: gambar ?? this.gambar,
+        teks: teks ?? this.teks,
+        tglTerbit: tglTerbit ?? this.tglTerbit,
+        createdAt: createdAt ?? this.createdAt,
+        createdBy: createdBy ?? this.createdBy,
+        updatedAt: updatedAt ?? this.updatedAt,
+        updatedBy: updatedBy ?? this.updatedBy,
+      );
 
   @override
   List<Object?> get props => [
     idBerita,
-    jenisKegiatan,
+    judul,
     penulis,
     gambar,
     teks,
+    tglTerbit,
+    createdAt,
+    createdBy,
+    updatedAt,
+    updatedBy,
   ];
 }
