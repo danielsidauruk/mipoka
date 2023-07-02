99dae816cadec5308007c600dbfc483dc7f2b216
danielsidauruk <danielsidauruk11@gmail.com>
2023-07-02 15:59:13 +0700

    Update RevisiUsulanKegiatan

--- lib/mipoka/presentation/pages/pemeriksa/pemeriksa_pengajuan_usulan_kegiatan_3_page.dart
+++ lib/mipoka/presentation/pages/pemeriksa/pemeriksa_pengajuan_usulan_kegiatan_3_page.dart
@@ -1,6 +1,11 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mipoka/core/routes.dart';
 import 'package:mipoka/core/theme.dart';
+import 'package:mipoka/mipoka/presentation/bloc/revisi_usulan_bloc/revisi_usulan_bloc.dart';
+import 'package:mipoka/mipoka/presentation/widgets/custom_comment_for_table.dart';
 import 'package:mipoka/mipoka/presentation/widgets/open_file_picker_method.dart';
 import 'package:mipoka/mipoka/presentation/widgets/custom_button.dart';
 import 'package:mipoka/mipoka/presentation/widgets/custom_comment_field.dart';
@@ -28,6 +33,14 @@ class PemeriksaPengajuanUsulanKegiatan3Page extends StatefulWidget {
 
 class _PemeriksaPengajuanUsulanKegiatan3PageState
     extends State<PemeriksaPengajuanUsulanKegiatan3Page> {
+
+  @override
+  void initState() {
+    context.read<RevisiUsulanBloc>().add(
+        ReadRevisiUsulanEvent(idRevisiUsulan: widget.idRevisiUsulan));
+    super.initState();
+  }
+
   final TextEditingController _latarBelakangController =
       TextEditingController();
   final TextEditingController _tujuanKegiatanController =
@@ -45,6 +58,12 @@ class _PemeriksaPengajuanUsulanKegiatan3PageState
   final TextEditingController _perlengkapanDanPeralatanController =
       TextEditingController();
   final TextEditingController _penutupController = TextEditingController();
+  final TextEditingController _tertibAcaraController = TextEditingController();
+
+  final StreamController<bool> _postinganKegiatanStream = StreamController<bool>();
+  final StreamController<bool> _suratUndanganStream = StreamController<bool>();
+  final StreamController<bool> _linimasaKegiatanStream = StreamController<bool>();
+  final StreamController<bool> _fotoTempatKegiatanStream = StreamController<bool>();
 
   @override
   Widget build(BuildContext context) {
@@ -125,13 +144,122 @@ class _PemeriksaPengajuanUsulanKegiatan3PageState
 
                   const CustomFieldSpacer(),
 
-                  // tertibAcara != false ?
-                  buildTitle('Tertib Acara'),
-                  CustomIconButton(
-                    onTap: () => Navigator.pushNamed(context,
-                        pemeriksaPengajuanUsulanKegiatan3TertibAcaraPageRoute),
-                    text: 'Detail',
-                    icon: Icons.arrow_forward_ios,
+                  CustomCommentForTable(
+                    title: "TertibAcara",
+                    controller: _tertibAcaraController,
+                  ),
+
+                  SingleChildScrollView(
+                    scrollDirection: Axis.vertical,
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: DataTable(
+                        columnSpacing: 40,
+                        border: TableBorder.all(color: Colors.white),
+                        columns: const [
+                          DataColumn(
+                            label: Text(
+                              'No.',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                          DataColumn(
+                            label: Text(
+                              'Aktivitas',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                          DataColumn(
+                            label: Text(
+                              'Waktu Mulai',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                          DataColumn(
+                            label: Text(
+                              'Waktu Selesai',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                          DataColumn(
+                            label: Text(
+                              'Keterangan',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ],
+                        // rows: List<DataRow>.generate(usulanKegiatan.tertibAcara.length, (int index) {
+                        rows: List<DataRow>.generate(4, (int index) {
+                          // final tertibAcara = usulanKegiatan.tertibAcara[index];
+                          return DataRow(
+                            cells: [
+                              DataCell(
+                                Align(
+                                  alignment: Alignment.center,
+                                  child: Text(
+                                    '${index + 1}',
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ),
+                              ),
+                              DataCell(
+                                InkWell(
+                                  onTap: () {
+                                    Navigator.pushNamed(
+                                      context,
+                                      editTertibAcaraPageRoute,
+                                      // arguments: usulanKegiatan,
+                                    );
+                                  },
+                                  child: Align(
+                                    alignment: Alignment.center,
+                                    child: Text(
+                                      "tertibAcara.aktivitas",
+                                      textAlign: TextAlign.center,
+                                      style: const TextStyle(
+                                        color: Colors.blue,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              DataCell(
+                                Align(
+                                  alignment: Alignment.center,
+                                  child: Text(
+                                    "tertibAcara.waktuMulai",
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ),
+                              ),
+                              DataCell(
+                                Align(
+                                  alignment: Alignment.center,
+                                  child: Text(
+                                   " tertibAcara.waktuSelesai",
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ),
+                              ),
+                              DataCell(
+                                Align(
+                                  alignment: Alignment.center,
+                                  child: Text(
+                                   " tertibAcara.keterangan",
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          );
+                        }),
+                      ),
+                    ),
                   ),
 
                   const CustomFieldSpacer(),
@@ -161,6 +289,7 @@ class _PemeriksaPengajuanUsulanKegiatan3PageState
                   buildTitle('Postingan Kegiatan'),
                   buildDescription(
                       'Unggah spanduk / pamflet mengenai kegiatan yang ingin dilaksanakan.'),
+
                   CustomIconButton(
                     onTap: () => selectAndUploadFile(
                       'postingKegiatanButton',
