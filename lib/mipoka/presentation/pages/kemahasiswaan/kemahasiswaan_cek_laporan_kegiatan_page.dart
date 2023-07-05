33d1ed5499668ec3154fad83c3d3fbe3ae061245
danielsidauruk <danielsidauruk11@gmail.com>
2023-07-05 14:18:06 +0700

    Update Kemahasiswaan Laporan

--- lib/mipoka/presentation/pages/kemahasiswaan/kemahasiswaan_cek_laporan_kegiatan_page.dart
+++ lib/mipoka/presentation/pages/kemahasiswaan/kemahasiswaan_cek_laporan_kegiatan_page.dart
@@ -3,12 +3,14 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mipoka/core/constanst.dart';
 import 'package:mipoka/core/theme.dart';
 import 'package:mipoka/mipoka/presentation/bloc/laporan_bloc/laporan_bloc.dart';
+import 'package:mipoka/mipoka/presentation/bloc/usulan_kegiatan_bloc/usulan_kegiatan_bloc.dart';
 import 'package:mipoka/mipoka/presentation/widgets/custom_content_box.dart';
 import 'package:mipoka/mipoka/presentation/widgets/mipoka_custom_dropdown.dart';
 import 'package:mipoka/mipoka/presentation/widgets/custom_field_spacer.dart';
 import 'package:mipoka/mipoka/presentation/widgets/custom_mipoka_mobile_appbar.dart';
 import 'package:mipoka/mipoka/presentation/widgets/custom_mobile_title.dart';
 import 'package:mipoka/mipoka/presentation/widgets/kemahasiswaan/kemahasiswaan_custom_drawer.dart';
+import 'package:mipoka/mipoka/presentation/widgets/mipoka_custom_total_count.dart';
 
 class KemahasiswaanCekLaporanKegiatanPage extends StatefulWidget {
   const KemahasiswaanCekLaporanKegiatanPage({super.key});
@@ -21,9 +23,8 @@ class _KemahasiswaanCekLaporanKegiatanPageState extends State<KemahasiswaanCekLa
 
   @override
   void initState() {
+    context.read<LaporanBloc>().add(const ReadAllLaporanEvent());
     super.initState();
-    BlocProvider.of<LaporanBloc>(context, listen: false)
-        .add(ReadAllLaporanEvent());
   }
 
   @override
@@ -37,16 +38,18 @@ class _KemahasiswaanCekLaporanKegiatanPageState extends State<KemahasiswaanCekLa
           if (state is LaporanLoading) {
             return const Text('Loading');
           } else if (state is AllLaporanHasData) {
+            final laporanList = state.laporanList;
+
             return Padding(
               padding: const EdgeInsets.all(16.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  const CustomMobileTitle(
-                      text: 'Pengajuan - Cek Laporan Kegiatan'),
-                  const CustomFieldSpacer(),
-                  Expanded(
-                    child: CustomContentBox(
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    const CustomMobileTitle(
+                        text: 'Pengajuan - Cek Laporan Kegiatan'),
+                    const CustomFieldSpacer(),
+                    CustomContentBox(
                       children: [
                         buildTitle('Status'),
 
@@ -57,142 +60,154 @@ class _KemahasiswaanCekLaporanKegiatanPageState extends State<KemahasiswaanCekLa
 
                         const CustomFieldSpacer(),
 
-                        buildTitle('Total Laporan Kegiatan : 2'),
+                        MipokaCountText(total: laporanList.length),
 
                         const CustomFieldSpacer(height: 4.0),
 
-                        Expanded(
+                        SingleChildScrollView(
+                          scrollDirection: Axis.vertical,
                           child: SingleChildScrollView(
-                            scrollDirection: Axis.vertical,
-                            child: SingleChildScrollView(
-                              scrollDirection: Axis.horizontal,
-                              child: DataTable(
-                                columnSpacing: 40,
-                                border: TableBorder.all(color: Colors.white),
-                                columns: const [
-                                  DataColumn(
-                                    label: Text(
-                                      'No.',
-                                      style: TextStyle(fontWeight: FontWeight.bold),
-                                      textAlign: TextAlign.center,
-                                    ),
+                            scrollDirection: Axis.horizontal,
+                            child: DataTable(
+                              columnSpacing: 40,
+                              border: TableBorder.all(color: Colors.white),
+                              columns: const [
+                                DataColumn(
+                                  label: Text(
+                                    'No.',
+                                    style: TextStyle(fontWeight: FontWeight.bold),
+                                    textAlign: TextAlign.center,
                                   ),
-                                  DataColumn(
-                                    label: Text(
-                                      'Tanggal',
-                                      style: TextStyle(fontWeight: FontWeight.bold),
-                                      textAlign: TextAlign.center,
-                                    ),
+                                ),
+                                DataColumn(
+                                  label: Text(
+                                    'Tanggal',
+                                    style: TextStyle(fontWeight: FontWeight.bold),
+                                    textAlign: TextAlign.center,
                                   ),
-                                  DataColumn(
-                                    label: Text(
-                                      'Nama Kegiatan',
-                                      style: TextStyle(fontWeight: FontWeight.bold),
-                                      textAlign: TextAlign.center,
-                                    ),
+                                ),
+                                DataColumn(
+                                  label: Text(
+                                    'Nama Kegiatan',
+                                    style: TextStyle(fontWeight: FontWeight.bold),
+                                    textAlign: TextAlign.center,
                                   ),
-                                  DataColumn(
-                                    label: Text(
-                                      'File Proposal',
-                                      style: TextStyle(fontWeight: FontWeight.bold),
-                                      textAlign: TextAlign.center,
-                                    ),
+                                ),
+                                DataColumn(
+                                  label: Text(
+                                    'File Proposal',
+                                    style: TextStyle(fontWeight: FontWeight.bold),
+                                    textAlign: TextAlign.center,
                                   ),
-                                  DataColumn(
-                                    label: Text(
-                                      'Validasi Pengguna',
-                                      style: TextStyle(fontWeight: FontWeight.bold),
-                                      textAlign: TextAlign.center,
-                                    ),
+                                ),
+                                DataColumn(
+                                  label: Text(
+                                    'Validasi Pengguna',
+                                    style: TextStyle(fontWeight: FontWeight.bold),
+                                    textAlign: TextAlign.center,
                                   ),
-                                  DataColumn(
-                                    label: Text(
-                                      'Aksi',
-                                      style: TextStyle(fontWeight: FontWeight.bold),
-                                      textAlign: TextAlign.center,
-                                    ),
+                                ),
+                                DataColumn(
+                                  label: Text(
+                                    'Aksi',
+                                    style: TextStyle(fontWeight: FontWeight.bold),
+                                    textAlign: TextAlign.center,
                                   ),
-                                ],
-                                rows: List<DataRow>.generate(12, (int index) {
-                                  return DataRow(
-                                    cells: [
-                                      DataCell(
-                                        Align(
-                                          alignment: Alignment.center,
-                                          child: Text('${index + 1}',),
+                                ),
+                              ],
+                              rows: List<DataRow>.generate(laporanList.length, (int index) {
+                                final laporan = laporanList[index];
+
+                                return DataRow(
+                                  cells: [
+                                    DataCell(
+                                      Align(
+                                        alignment: Alignment.center,
+                                        child: Text('${index + 1}',),
+                                      ),
+                                    ),
+                                    DataCell(
+                                      Align(
+                                        alignment: Alignment.center,
+                                        child: Text(laporan.createdAt),
+                                      ),
+                                    ),
+                                    DataCell(
+                                      BlocBuilder<UsulanKegiatanBloc, UsulanKegiatanState>(
+                                        builder: (context, state) {
+                                          if (state is UsulanKegiatanLoading) {
+                                            return const Text("Loading ....");
+                                          } else if (state is UsulanKegiatanHasData) {
+                                            return Align(
+                                              alignment: Alignment.center,
+                                              child: Text(state.usulanKegiatan.namaKegiatan),
+                                            );
+                                          } else if (state is UsulanKegiatanError) {
+                                            return Text(state.message);
+                                          } else {
+                                            return const Text("MipokaUserBloc hasn't been triggered yet.");
+                                          }
+                                        },
+                                      ),
+                                    ),
+                                    DataCell(
+                                      Center(
+                                        child: InkWell(
+                                          onTap: () => print(laporan.fileLaporanKegiatan),
+                                          child: Image.asset(
+                                            'assets/icons/pdf.png',
+                                            width: 24,
+                                          ),
                                         ),
                                       ),
-                                      DataCell(
-                                        Align(
-                                          alignment: Alignment.center,
-                                          child: Text('${index + 1}2 Mei 2023',),
+                                    ),
+                                    DataCell(
+                                      Center(
+                                        child: laporan.statusLaporan == "diterima" ?
+                                        Image.asset(
+                                          'assets/icons/approve.png',
+                                          width: 24,
+                                        ) :
+                                        Image.asset(
+                                          'assets/icons/close.png',
+                                          width: 24,
                                         ),
                                       ),
-                                      DataCell(
-                                        Align(
-                                          alignment: Alignment.center,
-                                          child: Text('Kegiatan - ${index + 1}',),
-                                        ),
-                                      ),
-                                      DataCell(
-                                        Center(
-                                          child: InkWell(
+                                    ),
+                                    DataCell(
+                                      Row(
+                                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                        children: [
+                                          InkWell(
                                             onTap: () {},
                                             child: Image.asset(
-                                              'assets/icons/pdf.png',
+                                              'assets/icons/approve.png',
                                               width: 24,
                                             ),
                                           ),
-                                        ),
-                                      ),
-                                      DataCell(
-                                        Center(
-                                          child: index % 2 == 0 ?
-                                          Image.asset(
-                                            'assets/icons/approve.png',
-                                            width: 24,
-                                          ) :
-                                          Image.asset(
-                                            'assets/icons/close.png',
-                                            width: 24,
+
+                                          const SizedBox(width: 8.0,),
+
+                                          InkWell(
+                                            onTap: () {},
+                                            child: Image.asset(
+                                              'assets/icons/close.png',
+                                              width: 24,
+                                            ),
                                           ),
-                                        ),
+                                        ],
                                       ),
-                                      DataCell(
-                                        Row(
-                                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                          children: [
-                                            InkWell(
-                                              onTap: () {},
-                                              child: Image.asset(
-                                                'assets/icons/approve.png',
-                                                width: 24,
-                                              ),
-                                            ),
-
-                                            const SizedBox(width: 8.0,),
-
-                                            InkWell(
-                                              onTap: () {},
-                                              child: Image.asset(
-                                                'assets/icons/close.png',
-                                                width: 24,
-                                              ),
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                    ],
-                                  );
-                                }),
-                              ),
+                                    ),
+                                  ],
+                                );
+                              }),
                             ),
                           ),
                         ),
                       ],
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             );
           } else if (state is LaporanError) {
