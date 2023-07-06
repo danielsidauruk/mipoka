3a947839a61cde59cb8e44aacfa30ca4dc70366d
danielsidauruk <danielsidauruk11@gmail.com>
2023-07-06 12:04:10 +0700

    Add Download Service for Multiplatform

---
+++ lib/domain/utils/download_service.dart
@@ -0,0 +1,57 @@
+import 'package:mipoka/mipoka/presentation/widgets/mipoka_custom_toast.dart';
+import 'package:universal_html/html.dart' as html;
+import 'package:dio/dio.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+abstract class DownloadService {
+  Future<void> download({required String url});
+}
+
+class WebDownloadService implements DownloadService {
+  @override
+  Future<void> download({required String url}) async {
+    html.window.open(url, "_blank");
+  }
+}
+
+class MobileDownloadService implements DownloadService {
+  @override
+  Future<void> download({required String url}) async {
+    // requests permission for downloading the file
+    bool hasPermission = await _requestWritePermission();
+    if (!hasPermission) return;
+
+    // gets the directory where we will download the file.
+    var dir = await getApplicationDocumentsDirectory();
+
+    // You should put the name you want for the file here.
+    // Take in account the extension.
+    String fileName = 'myFile';
+
+    // downloads the file
+    Dio dio = Dio();
+    // await dio.download(url, "${dir.path}/$fileName");
+
+    await Dio().download(
+      url,
+      "${dir.path}/$fileName}",
+      onReceiveProgress: (received, total) {
+        if (total != -1) {
+          // mipokaCustomToast("${(received / total * 100).toStringAsFixed(0)}%");
+          print("${(received / total * 100).toStringAsFixed(0)}%");
+          // You can build a progress bar feature too.
+        }
+      },
+    );
+    mipokaCustomToast("File is saved to the download folder.");
+
+
+  }
+
+  // requests storage permission
+  Future<bool> _requestWritePermission() async {
+    await Permission.storage.request();
+    return await Permission.storage.request().isGranted;
+  }
+}
--- lib/mipoka/presentation/pages/pengguna/import_peserta_laporan_page.dart
+++ lib/mipoka/presentation/pages/pengguna/import_peserta_laporan_page.dart
@@ -1,10 +1,15 @@
+import 'dart:io';
+import 'package:universal_html/html.dart' as html;
 import 'package:dio/dio.dart';
 import 'package:downloads_path_provider_28/downloads_path_provider_28.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mipoka/core/constanst.dart';
 import 'package:mipoka/core/theme.dart';
 import 'package:mipoka/domain/utils/download_file.dart';
+import 'package:mipoka/domain/utils/download_service.dart';
+import 'package:mipoka/domain/utils/get_device_info.dart';
 import 'package:mipoka/mipoka/domain/entities/peserta_kegiatan_laporan.dart';
 import 'package:mipoka/mipoka/domain/entities/rincian_biaya_kegiatan.dart';
 import 'package:mipoka/mipoka/presentation/bloc/peserta_kegiatan_laporan_bloc/peserta_kegiatan_laporan_bloc.dart';
@@ -74,12 +79,16 @@ class _ImportPesertaLaporanPageState extends State<ImportPesertaLaporanPage> {
                     mainAxisAlignment: MainAxisAlignment.end,
                     children: [
                       CustomMipokaButton(
-                        onTap: () {
+                        onTap: () async {
                           // downloadFile(pesertaKegiatanTemplate);
                           downloadFileWithDio(
                             url: pesertaKegiatanTemplate,
                             fileName: "pesertaKegiatan.xlsx"
                           );
+                          // getDeviceInfo();
+                          // DownloadService downloadService =
+                          // kIsWeb ? WebDownloadService() : MobileDownloadService();
+                          // await downloadService.download(url: pesertaKegiatanTemplate);
                         },
                         text: 'Unduh Templat',
                       ),
@@ -134,34 +143,38 @@ class _ImportPesertaLaporanPageState extends State<ImportPesertaLaporanPage> {
 
   Future<void> downloadFileWithDio({required String url, required String fileName}) async {
     try {
-      Map<Permission, PermissionStatus> statuses = await [
-        Permission.storage,
-        // Add more permissions to request here.
-      ].request();
+      if (Platform.isAndroid) {
+        Map<Permission, PermissionStatus> statuses = await [
+          Permission.storage,
+          // Add more permissions to request here.
+        ].request();
 
-      if (statuses[Permission.storage]!.isGranted) {
-        var dir = await DownloadsPathProvider.downloadsDirectory;
-        if (dir != null) {
-          // String fileName = url.split('/').last;
-          String savePath = "${dir.path}/$fileName";
-          print(savePath);
-          // Output: /storage/emulated/0/Download/file.pdf
+        if (statuses[Permission.storage]!.isGranted) {
+          var dir = await DownloadsPathProvider.downloadsDirectory;
+          if (dir != null) {
+            // String fileName = url.split('/').last;
+            String savePath = "${dir.path}/$fileName";
+            print(savePath);
+            // Output: /storage/emulated/0/Download/file.pdf
 
-          await Dio().download(
-            url,
-            savePath,
-            onReceiveProgress: (received, total) {
-              if (total != -1) {
-                // mipokaCustomToast("${(received / total * 100).toStringAsFixed(0)}%");
-                print("${(received / total * 100).toStringAsFixed(0)}%");
-                // You can build a progress bar feature too.
-              }
-            },
-          );
-          mipokaCustomToast("File is saved to the download folder.");
+            await Dio().download(
+              url,
+              savePath,
+              onReceiveProgress: (received, total) {
+                if (total != -1) {
+                  print("${(received / total * 100).toStringAsFixed(0)}%");
+                  // return mipokaCustomToast("${(received / total * 100).toStringAsFixed(0)}%");
+                  // You can build a progress bar feature too.
+                }
+              },
+            );
+            mipokaCustomToast("File is saved to the download folder.");
+          }
+        } else {
+          mipokaCustomToast("No permission to read and write.");
         }
-      } else {
-        mipokaCustomToast("No permission to read and write.");
+      } else if (kIsWeb) {
+        html.window.open(url, "_blank");
       }
     } catch (e) {
       mipokaCustomToast("Error while downloading file: $e");
@@ -169,3 +182,9 @@ class _ImportPesertaLaporanPageState extends State<ImportPesertaLaporanPage> {
     }
   }
 }
+
+// Future<void> _downloadFile({}) async {
+//   DownloadService downloadService =
+//   kIsWeb ? WebDownloadService() : MobileDownloadService();
+//   await downloadService.download(url: url);
+// }
