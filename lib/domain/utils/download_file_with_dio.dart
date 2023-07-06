e686c9eaf8f0c6ee3203fd68edce0266a12ad5f6
danielsidauruk <danielsidauruk11@gmail.com>
2023-07-06 12:53:21 +0700

    Add Download for Multiplatform

---
+++ lib/domain/utils/download_file_with_dio.dart
@@ -0,0 +1,50 @@
+import 'dart:io';
+import 'package:universal_html/html.dart' as html;
+import 'package:dio/dio.dart';
+import 'package:downloads_path_provider_28/downloads_path_provider_28.dart';
+import 'package:flutter/foundation.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:mipoka/mipoka/presentation/widgets/mipoka_custom_toast.dart';
+
+Future<void> downloadFileWithDio({
+  required String url,
+  required String fileName
+}) async {
+  try {
+    if (kIsWeb){
+      html.window.open(url, "_blank");
+    } else if (Platform.isAndroid) {
+      Map<Permission, PermissionStatus> statuses = await [
+        Permission.storage,
+        // Add more permissions to request here.
+      ].request();
+
+      if (statuses[Permission.storage]!.isGranted) {
+        var dir = await DownloadsPathProvider.downloadsDirectory;
+        if (dir != null) {
+          // String fileName = url.split('/').last;
+          String savePath = "${dir.path}/$fileName";
+          print(savePath);
+          // Output: /storage/emulated/0/Download/file.pdf
+
+          await Dio().download(
+            url,
+            savePath,
+            onReceiveProgress: (received, total) {
+              if (total != -1) {
+                print("${(received / total * 100).toStringAsFixed(0)}%");
+                // return mipokaCustomToast("${(received / total * 100).toStringAsFixed(0)}%");
+                // You can build a progress bar feature too.
+              }
+            },
+          );
+          mipokaCustomToast("File is saved to the download folder.");
+        }
+      } else {
+        mipokaCustomToast("No permission to read and write.");
+      }
+    }
+  } catch (e) {
+    mipokaCustomToast(e.toString());
+  }
+}
--- lib/mipoka/presentation/pages/pengguna/import_peserta_laporan_page.dart
+++ lib/mipoka/presentation/pages/pengguna/import_peserta_laporan_page.dart
@@ -1,28 +1,16 @@
-import 'dart:io';
-import 'package:universal_html/html.dart' as html;
-import 'package:dio/dio.dart';
-import 'package:downloads_path_provider_28/downloads_path_provider_28.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mipoka/core/constanst.dart';
 import 'package:mipoka/core/theme.dart';
-import 'package:mipoka/domain/utils/download_file.dart';
-import 'package:mipoka/domain/utils/download_service.dart';
-import 'package:mipoka/domain/utils/get_device_info.dart';
+import 'package:mipoka/domain/utils/download_file_with_dio.dart';
 import 'package:mipoka/mipoka/domain/entities/peserta_kegiatan_laporan.dart';
-import 'package:mipoka/mipoka/domain/entities/rincian_biaya_kegiatan.dart';
 import 'package:mipoka/mipoka/presentation/bloc/peserta_kegiatan_laporan_bloc/peserta_kegiatan_laporan_bloc.dart';
-import 'package:mipoka/mipoka/presentation/widgets/mipoka_custom_toast.dart';
-import 'package:mipoka/mipoka/presentation/widgets/open_file_picker_method.dart';
 import 'package:mipoka/mipoka/presentation/widgets/custom_button.dart';
 import 'package:mipoka/mipoka/presentation/widgets/custom_content_box.dart';
-import 'package:mipoka/mipoka/presentation/widgets/custom_field_picker.dart';
 import 'package:mipoka/mipoka/presentation/widgets/custom_drawer.dart';
 import 'package:mipoka/mipoka/presentation/widgets/custom_field_spacer.dart';
 import 'package:mipoka/mipoka/presentation/widgets/custom_mipoka_mobile_appbar.dart';
 import 'package:mipoka/mipoka/presentation/widgets/custom_mobile_title.dart';
-import 'package:permission_handler/permission_handler.dart';
 
 class ImportPesertaLaporanPage extends StatefulWidget {
   const ImportPesertaLaporanPage({
@@ -79,18 +67,10 @@ class _ImportPesertaLaporanPageState extends State<ImportPesertaLaporanPage> {
                     mainAxisAlignment: MainAxisAlignment.end,
                     children: [
                       CustomMipokaButton(
-                        onTap: () async {
-                          // downloadFile(pesertaKegiatanTemplate);
-                          // downloadFileWithDio(
-                          //   url: pesertaKegiatanTemplate,
-                          //   fileName: "pesertaKegiatan.xlsx"
-                          // );
-                          html.window.open(pesertaKegiatanTemplate, "_blank");
-                          // getDeviceInfo();
-                          // DownloadService downloadService =
-                          // kIsWeb ? WebDownloadService() : MobileDownloadService();
-                          // await downloadService.download(url: pesertaKegiatanTemplate);
-                        },
+                        onTap: () async => downloadFileWithDio(
+                            url: pesertaKegiatanTemplate,
+                            fileName: "pesertaKegiatan.xlsx"
+                        ),
                         text: 'Unduh Templat',
                       ),
                     ],
@@ -140,47 +120,6 @@ class _ImportPesertaLaporanPageState extends State<ImportPesertaLaporanPage> {
         ),
       ),
     );
-  }
-
-  Future<void> downloadFileWithDio({required String url, required String fileName}) async {
-    try {
-      if (Platform.isAndroid) {
-        Map<Permission, PermissionStatus> statuses = await [
-          Permission.storage,
-          // Add more permissions to request here.
-        ].request();
-
-        if (statuses[Permission.storage]!.isGranted) {
-          var dir = await DownloadsPathProvider.downloadsDirectory;
-          if (dir != null) {
-            // String fileName = url.split('/').last;
-            String savePath = "${dir.path}/$fileName";
-            print(savePath);
-            // Output: /storage/emulated/0/Download/file.pdf
-
-            await Dio().download(
-              url,
-              savePath,
-              onReceiveProgress: (received, total) {
-                if (total != -1) {
-                  print("${(received / total * 100).toStringAsFixed(0)}%");
-                  // return mipokaCustomToast("${(received / total * 100).toStringAsFixed(0)}%");
-                  // You can build a progress bar feature too.
-                }
-              },
-            );
-            mipokaCustomToast("File is saved to the download folder.");
-          }
-        } else {
-          mipokaCustomToast("No permission to read and write.");
-        }
-      } else if (kIsWeb) {
-        html.window.open(url, "_blank");
-      }
-    } catch (e) {
-      mipokaCustomToast("Error while downloading file: $e");
-      print(e);
-    }
   }
 }
 
